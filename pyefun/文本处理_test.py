93f59bb9ddcbb8564ebdf1f01460a6d9d01bb2be
吴烜 <wuxuan.ecios@gmail.com>
2021-05-22 14:02:14 -0700

    尝试重现问题

---
+++ pyefun/文本处理_test.py
@@ -0,0 +1,15 @@
+import unittest
+
+from .文本处理 import *
+
+
+class Test文本处理(unittest.TestCase):
+
+    def test_测试一下ide无法直接调试的问题(self):
+        pass
+        data = 测试一下ide无法直接调试的问题()
+
+
+def 测试一下ide无法直接调试的问题():
+    print("测试一下ide无法直接调试的问题")
+
